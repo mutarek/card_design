bec167d5ce9abf8a930c7c12618e1a6f9c62f6ca
tarek <mohiuddintarek@gmail.com>
2023-10-26 11:26:56 +0600

    card design added

---
+++ app/src/main/java/com/example/dummyprojects/RegestrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dummyprojects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegestrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regestration)
+    }
+}
